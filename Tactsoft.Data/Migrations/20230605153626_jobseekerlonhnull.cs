6c3846bff80f08a2c7349c3b156c82516c58eb2b
khpolash <khpolashbd@gmail.com>
2024-05-07 13:36:30 +0600

    Add project files.

---
+++ Tactsoft.Data/Migrations/20230605153626_jobseekerlonhnull.cs
@@ -0,0 +1,216 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Tactsoft.Data.Migrations
+{
+    public partial class jobseekerlonhnull : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "JobseekerId",
+                table: "PersonalDetails",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+
+            migrationBuilder.UpdateData(
+                table: "CompanySizes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Districts",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "IndustryTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "JobCategories",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "OrganizationTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Readings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Relatives",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ResumeReceivings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ServiceTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Speakings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Thanas",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Writings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, -8, 0, 0, 0)));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "JobseekerId",
+                table: "PersonalDetails",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "CompanySizes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Countries",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Districts",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "IndustryTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "JobCategories",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "OrganizationTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Readings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Relatives",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ResumeReceivings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "ServiceTypes",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Speakings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Thanas",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+
+            migrationBuilder.UpdateData(
+                table: "Writings",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "CreatedDateUtc",
+                value: new DateTimeOffset(new DateTime(2023, 2, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), new TimeSpan(0, 6, 0, 0, 0)));
+        }
+    }
+}
